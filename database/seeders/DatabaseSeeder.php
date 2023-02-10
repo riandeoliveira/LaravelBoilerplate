a78b477465451a1a7e2380ea6a387cb0aac019a7
Rian Oliveira <rian.oliveira@rehagro.edu.br>
2023-02-10 10:37:43 -0300

    refactor: update factories and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-// use App\Models\User;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder {
@@ -15,11 +15,6 @@ class DatabaseSeeder extends Seeder {
    * @return void
    */
   public function run(): void {
-    // User::factory(10)->create();
-
-    // User::factory()->create([
-    //   'name' => 'Test User',
-    //   'email' => 'test@example.com',
-    // ]);
+    User::factory(5)->create();
   }
 }

841e7405ccd4ab917aaac56385754b1b2994e146
Rian Oliveira <rian.oliveira@rehagro.edu.br>
2023-02-09 21:41:54 -0300

    docs: add Controller.php documentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,6 +9,33 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *   title="<PROJECT_NAME>",
+ *   description="<DESCRIPTION>",
+ *   version="0.0.1",
+ *
+ *   @OA\Contact(
+ *     name="<AUTHOR>",
+ *     email="<EMAIL>",
+ *     url="https://github.com/<GITHUB_USERNAME>"
+ *   ),
+ *
+ *   @OA\License(
+ *     name="Apache 2.0",
+ *     url="https://www.apache.org/licenses/LICENSE-2.0.html"
+ *   ),
+ *
+ *   @OA\Server(
+ *     url="http://localhost:8000",
+ *     description="Local development server."
+ *   ),
+ *
+ *   @OA\PathItem(
+ *     path="/api"
+ *   )
+ * ),
+ */
 class Controller extends BaseController {
   use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }

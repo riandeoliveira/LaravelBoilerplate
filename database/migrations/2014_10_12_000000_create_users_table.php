085e2caa270367670693b4076e4c53862c9e57ae
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-02-12 18:02:05 -0300

    feat: add 'username' field on users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,10 +23,11 @@ return new class () extends Migration {
   public function up(): void {
     Schema::create('users', function(Blueprint $table): void {
       $table->id();
+      $table->string('username')->unique();
       $table->string('name');
       $table->string('email')->unique();
-      $table->timestamp('email_verified_at')->nullable();
       $table->string('password');
+      $table->timestamp('email_verified_at')->nullable();
       $table->rememberToken();
       $table->timestamps();
     });
